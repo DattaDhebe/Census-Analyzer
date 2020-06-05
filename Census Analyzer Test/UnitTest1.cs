b182e327eff2e294841027770e33293453d95c7c
DattaDhebe <dattadhebe75@gmail.com>
2020-06-05 13:30:37 +0530

    Welcome To Census Analyzer

---
+++ Census Analyzer Test/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace Census_Analyzer_Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
---
+++ Census Analyzer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Census_Analyzer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
